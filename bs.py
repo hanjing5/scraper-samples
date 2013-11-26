8387675051314bd0a8f08e2665c0f6347989b739
hjing <hjing@walmartlabs.com>
2013-11-26 00:08:27 -0800

    working export

--- bs.py
+++ bs.py
@@ -1,10 +1,8 @@
 from bs4 import BeautifulSoup
 import urllib2
+import json
 
-u = "http://www.yelp.com/search?find_desc=bar+with+live+music&find_loc=San+Francisco%2C+CA&ns=1#find_desc=hipster+bars"
-
-html_doc = urllib2.urlopen(u).read()
-soup = BeautifulSoup(html_doc)
+URL = "http://www.yelp.com/search?find_desc=bar+with+live+music&find_loc=San+Francisco%2C+CA&ns=1#find_desc=hipster+bars"
 
 def initSchema():
     """
@@ -23,7 +21,7 @@ def clean(s):
     """
     return s.replace('\t','').replace('\n','')
 
-def getData(tag, h):
+def getData(soup, tag, h):
     """Get content(text) from an BeautifulSoup object
     given a base tag and an attribute in the form of a hash
     
@@ -33,23 +31,37 @@ def getData(tag, h):
       - `tag`: html tag name
       - `h`: hash representing the attribute name and value
     """
-    d = data.find_all(tag, h)
+    d = soup.find_all(tag, h)
     if len(d) > 0:
         return str(clean(d.pop().contents[0]))
     return None
 
-data_seg = soup.find_all('div',{'class':'biz-listing-large'})
-for data in data_seg:
-    schema = initSchema()
 
-    schema['neighborhood'] = getData('span', {'class':'neighborhood-str-list'})
-    schema['address'] = getData('address',{})
-    schema['name'] = getData('a',{'class':'biz-name'})
+def parse(url):
+    """Given an URL u, fetch all data we want into an array of objects
+    """
+    html_doc = urllib2.urlopen(url).read()
+    soup = BeautifulSoup(html_doc)
+    results = []
 
-    # ratings is special
-    rating_data = data.find_all('div',{'class':'rating-large'})
-    if len(rating_data) > 0:
-        rating = rating_data.pop().contents
-        schema['rating'] = rating[1]['title']
-    print schema
+    data_seg = soup.find_all('div',{'class':'biz-listing-large'})
 
+    for data in data_seg:
+        schema = initSchema()
+
+        schema['neighborhood'] = getData(data,'span', {'class':'neighborhood-str-list'})
+        schema['address'] = getData(data,'address',{})
+        schema['name'] = getData(data,'a',{'class':'biz-name'})
+
+        # ratings is special
+        rating_data = data.find_all('div',{'class':'rating-large'})
+        if len(rating_data) > 0:
+            rating = rating_data.pop().contents
+            schema['rating'] = rating[1]['title']
+        results.append(schema)
+    return {'results':results}
+
+if __name__ == "__main__":
+    # Export the file
+    with open("yelp-results.json","w") as f:
+        f.write(json.dumps(parse(URL)))
