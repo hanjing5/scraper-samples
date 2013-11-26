18c8eaa5078c12ef748d00998ed11c9176e57bc6
hjing <hjing@walmartlabs.com>
2013-11-25 23:57:12 -0800

    first version with functional scraping capabilities for one page

--- bs.py
+++ bs.py
@@ -1,43 +1,55 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-
 u = "http://www.yelp.com/search?find_desc=bar+with+live+music&find_loc=San+Francisco%2C+CA&ns=1#find_desc=hipster+bars"
 
 html_doc = urllib2.urlopen(u).read()
-
-
-#print html_doc
 soup = BeautifulSoup(html_doc)
 
-#for link in soup.find_all('a'):
-#    print(link.get('href'))
-
-schema = {
+def initSchema():
+    """
+        Base schema object we will use to hold the translated HTML
+    """
+    return {
         'name': None,
         'rating':None,
-        'neightborhood':None,
+        'neighborhood':None,
         'address':None
         }
 
+def clean(s):
+    """
+        Give a string, replace all none useful characters
+    """
+    return s.replace('\t','').replace('\n','')
+
 def getData(tag, h):
+    """Get content(text) from an BeautifulSoup object
+    given a base tag and an attribute in the form of a hash
+    
+    ex: <div class="foo"> will be ('div', {'class':'foo'})
+
+    :Parameters:
+      - `tag`: html tag name
+      - `h`: hash representing the attribute name and value
+    """
     d = data.find_all(tag, h)
     if len(d) > 0:
-        return d.pop().contents
+        return str(clean(d.pop().contents[0]))
     return None
 
 data_seg = soup.find_all('div',{'class':'biz-listing-large'})
 for data in data_seg:
+    schema = initSchema()
 
-    neighborhood_d = data.find_all('span',{'class':'neighborhood-str-list'})
-    if len(neighborhood_d) > 0:
-        neightborhood = d.pop().contents
+    schema['neighborhood'] = getData('span', {'class':'neighborhood-str-list'})
+    schema['address'] = getData('address',{})
+    schema['name'] = getData('a',{'class':'biz-name'})
 
-    address_d = data.find_all('address',{'class':'neighborhood-str-list'})
-    if len(address_d) > 0:
-        address = address_d.pop().contents
-
-    address_d = data.find_all('address',{'class':'neighborhood-str-list'})
-    if len(address_d) > 0:
-        address = address_d.pop().contents
+    # ratings is special
+    rating_data = data.find_all('div',{'class':'rating-large'})
+    if len(rating_data) > 0:
+        rating = rating_data.pop().contents
+        schema['rating'] = rating[1]['title']
+    print schema
 
